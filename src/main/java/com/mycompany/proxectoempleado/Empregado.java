86dcbc40ab2f691e52314fa61b8540bcc71df1ac
Nerea Trillo <a21nereatp@iessanclemente.net>
2022-06-09 21:54:11 +0200

    creada clase con atributos, constructor, to string e getters e setters

--- src/main/java/com/mycompany/proxectoempleado/Empregado.java
+++ src/main/java/com/mycompany/proxectoempleado/Empregado.java
@@ -10,4 +10,68 @@ package com.mycompany.proxectoempleado;
  */
 public class Empregado {
     
+    private int id;
+
+    private String nome;
+
+    private String apelido1;
+
+    private String apelido2;
+
+    private double salario;
+
+    public Empregado(int id, String nome, String apelido1, String apelido2, double salario) {
+        this.id = id;
+        this.nome = nome;
+        this.apelido1 = apelido1;
+        this.apelido2 = apelido2;
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getApelido2() {
+        return apelido2;
+    }
+
+    public void setApelido2(String apelido2) {
+        this.apelido2 = apelido2;
+    }
+
+    public String getApelido1() {
+        return apelido1;
+    }
+
+    public void setApelido1(String apelido1) {
+        this.apelido1 = apelido1;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Empregado{" + "id=" + id + ", nome=" + nome + ", apelido1=" + apelido1 + ", apelido2=" + apelido2 + ", salario=" + salario + '}';
+    }
+
 }
